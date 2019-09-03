041ce9028b89c4cabe8ab4e72021df403cc28e4e
Skydeke <david-metzler@web.de>
2019-09-03 12:35:10 +0200

    Working JavaFX

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,39 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    Button btn;
+    @FXML
+    TextArea errorArea;
+
+    public Controller(){
+        //@FXML-Fields are not initialized
+        System.out.println("Initiated Controller instance");
+    }
+
+    public void onBtnPress(ActionEvent buttonEvent){
+        errorArea.setText("Pressed: " + btn.getText());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //@FXML-Fields are initialized
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                onBtnPress(actionEvent);
+            }
+        });
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,19 +5,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 public class Main extends Application {
+    static final FXMLLoader loader = new FXMLLoader();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("mainUI.fxml"));
+        Controller controller = loader.getController();
+        loader.setController(controller);
+        Parent root = loader.load(getClass().getResource("mainUI.fxml"));
         primaryStage.setTitle("Title");
-        primaryStage.setScene(new Scene(root, 800, 500));
+        Scene s = new Scene(root, 800, 500);
+        primaryStage.setScene(s);
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(Main.class);
     }
 }
